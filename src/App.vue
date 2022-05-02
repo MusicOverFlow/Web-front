2e3b1a2a1bf356d3665526615a89c5750d311b8e
Quentin ROUVILLE <qrouville@posteo.net>
2022-05-02 10:06:16 +0200

    Improve App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,13 @@
   <FooterComponent/>
 </template>
 
-<style>
-/*#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}*/
-</style>
 <script>
 import NavbarComponent from "@/components/NavbarComponent";
 import FooterComponent from "@/components/FooterComponent";
 export default {
-  components: {FooterComponent, NavbarComponent}
+  components: {
+    FooterComponent,
+    NavbarComponent
+  }
 }
 </script>
